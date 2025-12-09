2ba30d9b4a4e0052922543d93220d009923a6c02
graffuxx <graffuxxx@gmail.com>
2025-12-09 20:38:40 +0100

    Fix LayoutWrapper without css module

--- src/components/Layout/LayoutWrapper.tsx
+++ src/components/Layout/LayoutWrapper.tsx
@@ -1,13 +1,21 @@
-// components/Layout/LayoutWrapper.tsx
 'use client';
 
+import React from 'react';
 import TickerBar from './TickerBar';
 
-export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function LayoutWrapper({ children }: Props) {
   return (
-    <>
+    <div style={{ minHeight: '100vh' }}>
+      {/* News / Ticker oben unter der Navbar */}
       <TickerBar />
-      {children}
-    </>
+      {/* Platz für Header + Ticker, damit nichts überlappt */}
+      <div style={{ paddingTop: '110px' }}>
+        {children}
+      </div>
+    </div>
   );
 }
