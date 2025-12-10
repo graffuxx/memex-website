c45bbfe67d5cc720010b787272cfd901ee5b33c0
graffuxx <graffuxxx@gmail.com>
2025-12-10 11:06:28 +0100

    Restore wallet button in header, fix account wallet visibility, update header layout

--- src/components/Layout/ModernHeader.tsx
+++ src/components/Layout/ModernHeader.tsx
@@ -19,24 +19,22 @@ export default function ModernHeader() {
   return (
     <header className={`${styles.header} ${scrolled ? styles.scrolled : ''}`}>
       <div className={styles.inner}>
-        {/* Mobile: Burger links */}
-        <div className={styles.mobileOnly}>
-          <MobileMenu />
-        </div>
+        {/* Left: Mobile burger */}
+        <MobileMenu />
 
-        {/* Desktop: Navigation mittig/links */}
+        {/* Center: Navigation */}
         <nav className={styles.navbar}>
-          <a href="/" className={styles.navLink}>Home</a>
-          <a href="/presale" className={styles.navLink}>Presale</a>
-          <a href="/how-to-play" className={styles.navLink}>How to Play</a>
-          <a href="/whitepaper" className={styles.navLink}>Whitepaper</a>
-          <a href="/socials" className={styles.navLink}>Socials</a>
-          <a href="/nft-shop" className={styles.navLink}>NFT Shop</a>
-          <a href="/account" className={styles.navLink}>Account</a>
+          <a href="/">Home</a>
+          <a href="/presale">Presale</a>
+          <a href="/how-to-play">How to Play</a>
+          <a href="/whitepaper">Whitepaper</a>
+          <a href="/socials">Socials</a>
+          <a href="/nft-shop">NFT Shop</a>
+          <a href="/account">Account</a>
         </nav>
 
-        {/* Rechts: Wallet-Button – immer sichtbar */}
-        <div className={styles.walletWrapper}>
+        {/* Right: WALLET BUTTON (sichtbar!) */}
+        <div className={styles.walletArea}>
           <WalletButton />
         </div>
       </div>
